108369cc77fcb074457bab42bd8ff8cb05999259
SHARMILA47 <130386047+SHARMILA47@users.noreply.github.com>
2024-08-14 22:41:45 +0530

    Add files via upload

---
+++ Code/dashboard.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BIT Mail Block/Unblock Portal</title>
+        <link rel="icon" href="bit logo.png">
+    </head>
+
+    <body>
+        <br>
+        <br>
+        <br>
+        <center><h1>BANNARI AMMAN INSTITUTE OF TECHNOLOGY</h1></center>
+        <center><h2>Stay Ahead</h2></center>
+        <hr color="black">
+        <center>
+            
+            <p>Click on the link given below to know more about BIT</p>
+            <img src="bannari college logo.jpeg" width="100px"><br>
+            <a href="https://www.bitsathy.ac.in/">Bannari Amman Institute Of Technology</a>
+            <br>
+            <p>Click on the link given below to import CSV file</p>
+            <a href="'#">Import CSV</a>
+            
+            <br>
+            <hr color="black">
+            <h4>📍Bannari Amman Institute Of Technology</h4>
+            <h4>📞04295 226585</h4>
+
+            <p>Sathy - Bhavani State Highway, Alathukombai, Post, Sathyamangalam, Tamil Nadu 638401</p>
+        </center>
+    </body>
+</html>
+
+<style>
+body
+{
+     background-image:url('bg6.jpeg');
+     background-size:cover;
+}
+a{color:aquamarine;}
+h1{color:antiquewhite;}
+h2{color:antiquewhite;}
+h3{color:antiquewhite;}
+h4{color:antiquewhite;}
+p{color:antiquewhite;}
+</style>
